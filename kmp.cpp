f1a6a88df0ea59b5e8f5531df5d92f9a6f937a5d
Shirin <ShirinLin@iCloud.com>
2026-02-01 18:21:17 +0800

    kmp ing

---
+++ kmp.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+//前缀函数
+vector<int> next1(const string &p){
+    int m = p.size();
+    vector<int> next(m,0);
+    int len = 0,i = 1;
+    while(i < m){
+        
+    }
+    
+}
+//kmp
+
+
+int main() {
+    string text,pattern;
+    cout<<"请输入文本串：";
+    cin>>text;
+    cout<<"请输入模式串：";
+    cin>>pattern;
+    //kmp(text,pattern);
+
+    return 0;
+}
