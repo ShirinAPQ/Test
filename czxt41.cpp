40a4e6bbe2fb69468aa83981c80a6990c3953818
Shirin <ShirinLin@iCloud.com>
2026-06-12 10:14:30 +0800

    重构磁盘调度算法实现，优化随机请求生成及算法选择流程

--- czxt41.cpp
+++ czxt41.cpp
@@ -3,165 +3,155 @@
 #include <math.h>
 #include <time.h>
 
-#define MAX_NUM 20   // 最大磁道请求数
-#define MAX_CYL 200  // 磁盘最大磁道号(0~199)
+#define MAX 20    // 最大磁道请求数
+int track[MAX];   // 存放磁道序列
+int num;          // 磁道数量
+int curTrack;     // 当前磁头位置
 
-int request[MAX_NUM];  // 存储磁盘请求磁道序列
-int req_num;           // 实际请求数量
-int head_pos;          // 磁头初始位置
-
-// 随机生成磁盘请求序列
-void createRandomRequest()
+// 随机生成磁道序列
+void RandTrack()
 {
     srand((unsigned int)time(NULL));
-    printf("请输入磁盘请求数量(1-%d)：", MAX_NUM);
-    scanf("%d", &req_num);
-    for (int i = 0; i < req_num; i++)
+    printf("请输入要处理的磁道数：");
+    scanf("%d", &num);
+    printf("随机生成磁道号:\n");
+    for (int i = 0; i < num; i++)
     {
-        request[i] = rand() % MAX_CYL;
+        track[i] = rand() % 200; // 磁道范围 0~199
+        printf("%d ", track[i]);
     }
-    printf("随机生成的磁道请求序列：");
-    for (int i = 0; i < req_num; i++)
-    {
-        printf("%d ", request[i]);
-    }
-    printf("\n");
+    printf("数据生成成功!\n\n");
 }
 
-// FCFS 先来先服务算法
-void FCFS()
-{
-    printf("========== FCFS 先来先服务算法 ==========\n");
-    int cur = head_pos;
-    int total_move = 0;
-    printf("磁头访问顺序：%d", cur);
-    for (int i = 0; i < req_num; i++)
-    {
-        total_move += abs(cur - request[i]);
-        cur = request[i];
-        printf(" -> %d", cur);
-    }
-    printf("\n总寻道距离：%d\n", total_move);
-    printf("平均寻道长度：%.2f\n\n", (double)total_move / req_num);
-}
-
-// SSTF 最短寻道时间优先算法
+// 最短寻道时间优先 SSTF
 void SSTF()
 {
-    printf("========== SSTF 最短寻道时间优先算法 ==========\n");
-    int temp_req[MAX_NUM];
-    // 拷贝请求序列，避免修改原数据
-    for (int i = 0; i < req_num; i++)
-        temp_req[i] = request[i];
+    int temp[MAX], flag[MAX] = {0};
+    int total = 0, now = curTrack;
+    // 拷贝磁道
+    for (int i = 0; i < num; i++)
+        temp[i] = track[i];
 
-    int cur = head_pos;
-    int total_move = 0;
-    int count = 0;
-    int min_dis, min_idx;
-    printf("磁头访问顺序：%d", cur);
+    printf("排序后的磁道分布:\n");
+    for (int i = 0; i < num; i++)
+        printf("%d ", temp[i]);
+        printf("\n");
 
-    while (count < req_num)
+    printf("请输入当前磁道号:\n");
+    scanf("%d", &now);
+    curTrack = now;
+    printf("当前访问的磁道:%d\n", now);
+
+    for (int k = 0; k < num; k++)
     {
-        min_dis = MAX_CYL;
-        min_idx = -1;
-        // 查找距离当前磁头最近的未访问磁道
-        for (int i = 0; i < req_num; i++)
+        int min = 9999, pos = 0;
+        // 找距离当前磁道最近且未访问的磁道
+        for (int i = 0; i < num; i++)
         {
-            if (temp_req[i] != -1)
+            if (flag[i] == 0 && abs(temp[i] - now) < min)
             {
-                int dis = abs(cur - temp_req[i]);
-                if (dis < min_dis)
-                {
-                    min_dis = dis;
-                    min_idx = i;
-                }
+                min = abs(temp[i] - now);
+                pos = i;
             }
         }
-        // 访问该磁道
-        total_move += min_dis;
-        cur = temp_req[min_idx];
-        printf(" -> %d", cur);
-        temp_req[min_idx] = -1; // 标记已访问
-        count++;
+        flag[pos] = 1;
+        total += min;
+        now = temp[pos];
+        printf("当前访问的磁道:%d\n", now);
     }
-    printf("\n总寻道距离：%d\n", total_move);
-    printf("平均寻道长度：%.2f\n\n", (double)total_move / req_num);
+
+    printf("经过的总磁道数为:%d\n", total);
+    printf("移动的平均磁道数:%.2f\n\n", (double)total / num);
 }
 
-// SCAN 电梯算法(默认初始方向：磁道号增大)
+// 电梯算法 SCAN (默认向磁道增大方向)
 void SCAN()
 {
-    printf("========== SCAN 电梯算法(向磁道号增大方向) ==========\n");
-    int temp_req[MAX_NUM];
-    for (int i = 0; i < req_num; i++)
-        temp_req[i] = request[i];
+    int temp[MAX], flag[MAX] = {0};
+    int total = 0, now = curTrack;
+    for (int i = 0; i < num; i++)
+        temp[i] = track[i];
 
-    int cur = head_pos;
-    int total_move = 0;
-    int count = 0;
-    printf("磁头访问顺序：%d", cur);
+    printf("排序后的磁道分布:\n");
+    for (int i = 0; i < num; i++)
+        printf("%d ", temp[i]);
+        printf("\n");
 
-    // 第一步：向磁道号增大方向访问
-    while (count < req_num)
+    printf("请输入当前磁道号:\n");
+    scanf("%d", &now);
+    curTrack = now;
+    printf("当前访问的磁道:%d\n", now);
+
+    // 第一步：向磁道号增大方向移动
+    for (int i = 0; i < num; i++)
     {
-        int find = 0;
-        // 寻找当前位置右侧(更大磁道号)的请求
-        for (int i = 0; i < req_num; i++)
+        int select = -1;
+        for (int j = 0; j < num; j++)
         {
-            if (temp_req[i] != -1 && temp_req[i] >= cur)
+            if (flag[j] == 0 && temp[j] >= now)
             {
-                int dis = abs(cur - temp_req[i]);
-                total_move += dis;
-                cur = temp_req[i];
-                printf(" -> %d", cur);
-                temp_req[i] = -1;
-                count++;
-                find = 1;
-                break;
+                if (select == -1 || temp[j] < temp[select])
+                    select = j;
             }
         }
-        if (!find) break; // 右侧无请求，退出第一阶段
+        if (select == -1) break;
+        flag[select] = 1;
+        total += abs(temp[select] - now);
+        now = temp[select];
+        printf("当前访问的磁道:%d\n", now);
     }
 
-    // 第二步：向磁道号减小方向访问剩余请求
-    while (count < req_num)
+    // 第二步：向磁道号减小方向移动
+    for (int i = 0; i < num; i++)
     {
-        int find = 0;
-        // 寻找当前位置左侧(更小磁道号)的请求
-        for (int i = 0; i < req_num; i++)
+        int select = -1;
+        for (int j = 0; j < num; j++)
         {
-            if (temp_req[i] != -1 && temp_req[i] <= cur)
+            if (flag[j] == 0 && temp[j] <= now)
             {
-                int dis = abs(cur - temp_req[i]);
-                total_move += dis;
-                cur = temp_req[i];
-                printf(" -> %d", cur);
-                temp_req[i] = -1;
-                count++;
-                find = 1;
-                break;
+                if (select == -1 || temp[j] > temp[select])
+                    select = j;
             }
         }
-        if (!find) break;
+        if (select == -1) break;
+        flag[select] = 1;
+        total += abs(temp[select] - now);
+        now = temp[select];
+        printf("当前访问的磁道:%d\n", now);
     }
-    printf("\n总寻道距离：%d\n", total_move);
-    printf("平均寻道长度：%.2f\n\n", (double)total_move / req_num);
+
+    printf("经过的总磁道数为:%d\n", total);
+    printf("移动的平均磁道数:%.2f\n\n", (double)total / num);
 }
 
 int main()
 {
-    // 1. 生成随机磁道请求
-    createRandomRequest();
-    // 2. 输入磁头初始位置
-    printf("请输入磁头初始位置(0-%d)：", MAX_CYL - 1);
-    scanf("%d", &head_pos);
-    printf("=============================================\n\n");
+    int op;
+    RandTrack(); // 生成随机磁道
 
-    // 3. 执行三种调度算法
-    FCFS();
-    SSTF();
-    SCAN();
-
+    while (1)
+    {
+        printf("算法选择\n");
+        printf("1.最短寻道时间算法\n");
+        printf("2.扫描算法\n");
+        printf("0.退出程序\n");
+        printf("请输入你想使用的方法:");
+        scanf("%d", &op);
+        if (op == 0)
+            break;
+        switch (op)
+        {
+            case 1:
+                SSTF();
+                break;
+            case 2:
+                SCAN();
+                break;
+            default:
+                printf("输入错误，请重新选择！\n\n");
+                break;
+        }
+    }
     system("pause");
     return 0;
 }
